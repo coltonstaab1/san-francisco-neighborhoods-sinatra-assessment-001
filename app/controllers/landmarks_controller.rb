15eddca2db359f836153d862dcec307ea31c003b
coltonstaab1 <colton.staab@gmail.com>
2016-03-16 08:50:15 -0700

    Added landmarks edit feature

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,10 +12,30 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     if logged_in?
       @neighborhoods = Neighborhood.all
+      binding.pry
       erb :'landmarks/new'
     else
       redirect to '/login'
     end
+  end
+
+  get '/landmarks/:id/edit' do
+    redirect_if_not_logged_in
+    @landmark = Landmark.find(params[:id])
+    @neighborhoods = Neighborhood.all
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:name], address: params[:address], landmark_type: params[:landmark_type], neighborhood_id: params[:neighborhood_id])
+    redirect '/landmarks'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params[:name], address: params[:address], landmark_type: params[:landmark_type], neighborhood_id: params[:neighborhood_id])
+    binding.pry
+    redirect to "/landmarks/#{@landmark.id}"
   end
 
   get '/landmarks/:id' do
